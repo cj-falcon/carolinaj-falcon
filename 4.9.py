3465328cb0d7db2796cccf02d69577f87b4d7661
Carolina Falcon <cfalcona291@gmail.com>
2025-10-29 20:07:46 -0600

    4.9.py

--- 4.9.py
+++ 4.9.py
@@ -10,20 +10,22 @@
 #-------------------------------------------------------------------------------
 
 import turtle
+
 screen = turtle.Screen()
 screen.bgcolor("lightgreen")
-pen.color("hotpink")
-pen.pensize(5)
 
 t = turtle.Turtle()
-t.speed(5)
+t.pensize(4)
+t.speed(10)
 
+size = 20
 for i in range(5):
   for j in range(4):
     t.forward(50 + i + 20)
     t.left(90)
+  size += 20
   t.penup()
-  t.goto(-10 * (i+1), -10 * (i+1))
+  t.goto(-size/2, -size/2)
   t.pendown()
 
 turtle.done()
