ecf750670e5121ebc6424f5935fc3f2443d2a898
Carolina Falcon <cfalcona291@gmail.com>
2025-11-13 15:16:26 -0700

    Add files via upload

---
+++ 11.22problem10.py
@@ -0,0 +1,28 @@
+import sys
+
+s = "I love spom! Spom is my favorite food. Spom, spom, yum!"
+
+def replace(s, old, new):
+    """ Replace all occurrence of old with new in s string """
+    return new.join(s.split(old))
+
+
+def test(did_pass):
+    """ Print result of a test """
+    linenum = sys._getframe(1).f_lineno
+    if did_pass:
+        msg = "Test at line {0} is ok".format(linenum)
+    else:
+        msg = "Test at line {0} is FAILED".format(linenum)
+    print(msg)
+
+
+def test_suite():
+    """Run the suite of tests for code in this module """
+    test(replace("Mississippi", "i", "I") == "MIssIssIppI")
+    test(replace(s, "om", "am") ==
+    "I love spam! Spam is my favorite food. Spam, spam, yum!")
+    test(replace(s, "o", "a") ==
+    "I lave spam! Spam is my favarite faad. Spam, spam, yum!")
+
+test_suite() 
